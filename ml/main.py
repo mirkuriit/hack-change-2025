90eaea5107d42058e745149fa5c7400999b82bd9
dmiptrv0 <dmitri.p.sft@gmail.com>
2025-11-30 00:48:23 +0300

    Update main.py

--- ml/main.py
+++ ml/main.py
@@ -2,7 +2,13 @@ from model import *
 from tokenizer import *
 import torch
 import pickle
+import numpy as np
+from metrics import *
+from torch.utils.data import DataLoader
+from dataset import *
+import pandas as pd
 
+device = 'cpu'
 
 def predict(model, tokenizer, text, device='cpu'):
     tokens = tokenizer.encode(text)
@@ -11,16 +17,56 @@ def predict(model, tokenizer, text, device='cpu'):
     model.to(device)
     model.eval()
     with torch.no_grad():
+
         output = model(input_tensor)
 
     return output.squeeze(0).argmax().item()
 
+def get_metrics_by_train(model, table):
+    table = np.array(table)
+
+    test_dataset = CustomDataset(table)
+
+    test_dataloader = DataLoader(
+        test_dataset, shuffle=True, collate_fn=collate_fn_with_padding, batch_size=20)
+
+    f1 = F1macro(model, test_dataloader)
+    accuracy = F1macro(model, test_dataloader)
+
+    return f1, accuracy
+
+def predict_for_table(model, path_to_table, path_to_save):
+    table_df = pd.read_csv(path_to_table)
+    table = np.array(table_df)
+
+    val_dataset = CustomDataset(table, is_train=False)
+
+    val_dataloader = DataLoader(
+        val_dataset, shuffle=True, collate_fn=collate_fn_with_padding, batch_size=20)
+
+    predictions = []
+    target = []
+    model.eval()
+
+    with torch.no_grad():
+        for batch in val_dataloader:
+            logits = model(batch['input_ids'].to(device))
+            predictions.append(logits.argmax(dim=1))
+
+    predictions = torch.cat(predictions, dim=0)
+
+    table_df['predictions'] = predictions
+
+    table_df.to_csv(path_to_save, index=False)
+
+def predict_by_table(model, path_to_table, tokenizer, device='cpu'):
+    pass
 
 with open('save_data.pkl', 'rb') as file:
     save_data = pickle.load(file)
 
-model, word2int = save_data
+model, word2ind = save_data
+model.to('cpu')
 
-print(
-    predict(model, MyTokenizer(word2int), "Отличный товар! Это просто замечательно")
-)
+
+predict_for_table(model, "./test_first_1000.csv", "predicted_table")
