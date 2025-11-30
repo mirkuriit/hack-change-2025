6fa5f06cf7f5b037d070a48a64907c67d4f9096f
Dmitry <dmitri.p.sft@gmail.com>
2025-11-30 03:37:51 +0300

    ADD

--- ml/loader.py
+++ ml/loader.py
@@ -1,5 +1,6 @@
 import pickle
 import torch
+from model import *
 
 device = 'cpu'
 
@@ -8,6 +9,13 @@ with open('save_data_new.pkl', 'rb') as file:
 
 word2ind = save_data[0]
 
-model = torch.load("full_model.pt", map_location=device, weights_only=False)
+model = BaseModel(
+    hidden_dim=256,
+    vocab_size=len(word2ind),
+    num_classes=3,
+    lstm_layers=2,
+    aggregation_type='max+mean'
+    ).to(device)
+
+model.load_state_dict(torch.load("./full_model_state.pth", map_location=device))
 model.to(device)
-model.eval()
